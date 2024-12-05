78e3235dd8c97c6b1f9abc268794027fe2d2effd
junx21 <jxuche@myuax.com>
2024-12-05 10:31:56 +0100

    ej2

--- ej2/EJ2.cpp
+++ ej2/EJ2.cpp
@@ -1,3 +1,9 @@
+/*Ejercicio 2: Espacio de nombres repartido entre diferentes archivos
+ Crea un programa donde defines un espacio de nombres "Geometría" en dos archivos de código separados
+ (por ejemplo, "triángulo.cpp" y "círculo.cpp"). Cada archivo debe contener una función
+ (por ejemplo, "calcularArea") correspondiente a la geometría específica.
+ Finalmente, desde la función main en otro archivo, utiliza estas funciones.*/
+
 #include <iostream>
 #include "Circulo.h"
 #include "Triangulo.h"

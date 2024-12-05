788a1366322a9fc4843b5fa1684ae207a4f46e2c
junx21 <jxuche@myuax.com>
2024-12-05 10:30:28 +0100

    main

--- main .cpp
+++ main .cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 #include "ej1/Matematicas.h"
-#include "ej2/Circulo.h"
-#include "ej2/Triangulo.h"
+#include "ej2/EJ2.h"
 #include "ej3/Clase_Espacio.h"
 #include "ej4/Fisica.h"
+#include "ej5/STL.h"
 
 
 
 int main() {
+    int ejercicio = 0;
 
-    std::cout << "\nEjercicio 1\n" << std::endl;
-    std::cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
-    std::cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
+    std::cout << "Elija el ejercicio a ejecutar (1,2,3,4,5): ";
+    std::cin >> ejercicio;
 
-    std::cout << "\nEjercicio 2\n" << std::endl;
-    std::cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(5, 3) << std::endl;
-    std::cout << "Area del circulo: " << Geometria::calcularAreaCirculo(3) << std::endl;
+    switch (ejercicio) {
+        case 1: EJ1(); break;
+        case 2: EJ2(); break;
+        case 3: EJ3(); break;
+        case 4: EJ4(); break;
+        case 5: EJ5(); break;
 
-    std::cout << "\nEjercicio 3\n" << std::endl;
-    Geometria::Circulo c(5);
-    std::cout << "Area: " << c.calcularArea() << std::endl;
-    std::cout << "Perimetro: " << c.calcularPerimetro() << std::endl;
+        default:
+            std::cout << "EJ no encontrado" << std::endl;
+    }
 
-    std::cout << "\nEjercicio 4\n" << std::endl;
-    std::cout << "Energia: " << Ciencia::Fisica::calcularEnergia(10) << std::endl;
 
-    std::cout << "\nEjercicio 5\n" << std::endl;
 
     return 0;
 }
